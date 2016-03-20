d616a655e04c426b5bbbed3a1979c76970aa28fb
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-03-19 20:14:22 -0600

    error y success handled

--- src/Hagane/Hagane.php
+++ src/Hagane/Hagane.php
@@ -13,17 +13,27 @@ class Hagane {
 	}
 
 	public function get($url){
-		if(substr($url, 0) == '/'){
+		if(substr($url, 0, 1) == '/'){
 			$url = $this->apiurl . $url;
 		} else {
 			$url = $this->apiurl . '/' . $url;
 		}
+		
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_URL => $url,		
 			CURLOPT_USERAGENT => 'Hagane PHP SDK'
 		));
-		return curl_exec($curl);
+		$response = curl_exec($curl);
+		$json = json_decode($response);
+		
+		$jsonarr = (array)$json;
+		if(!empty($jsonarr['error'])){
+			return json_encode($json->error);
+		} elseif(!empty($jsonarr['success'])){
+			unset($json->success);
+			return $json->message;
+		}
 	}
 }
