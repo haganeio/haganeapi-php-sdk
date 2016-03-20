8ab7926e5c8c5579c48aaa7d4e40fb1d1e7bf1ae
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-03-19 19:31:54 -0600

    get using curl

--- src/Hagane/Hagane.php
+++ src/Hagane/Hagane.php
@@ -3,18 +3,27 @@ namespace Hagane;
 
 class Hagane {
 	private $apiurl;
-	private $curl;
-
+	
 	public function __construct($url = null){
 		$this->apiurl = $url;
-		$this->curl = curl_exec($url);
 	}
 
 	public function hello(){
 		return 'Hello World';
 	}
 
-	public get($url){
-		return curl_exec($curl);		
+	public function get($url){
+		if(substr($url, 0) == '/'){
+			$url = $this->apiurl . $url;
+		} else {
+			$url = $this->apiurl . '/' . $url;
+		}
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $url,		
+			CURLOPT_USERAGENT => 'Hagane PHP SDK'
+		));
+		return curl_exec($curl);
 	}
 }
