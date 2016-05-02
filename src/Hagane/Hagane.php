c6b91a1d0add5d1732ca7145ca4891b6e2e9f9d1
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-05-02 04:13:54 -0500

    Estructura de objetos para tener un retorno de la promesa con un estilo un poco mas Javascriptesco

--- src/Hagane/Hagane.php
+++ src/Hagane/Hagane.php
@@ -2,16 +2,12 @@
 namespace Hagane;
 
 class Hagane {
-	private $apiurl;
+	protected $apiurl;
 	
 	public function __construct($url = null){
 		$this->apiurl = $url;
 	}
-
-	public function hello(){
-		return 'Hello World';
-	}
-
+	
 	public function get($url, $accessToken = null, $getparams = null){
 		if(substr($url, 0, 1) == '/'){
 			$url = $this->apiurl . $url;
@@ -74,6 +70,10 @@ class Hagane {
 			CURLOPT_POSTFIELDS => json_encode($params)
 		));
 		$response = curl_exec($curl);
+		// echo '<pre>';
+		// echo $url.'<br>';
+		// var_dump($response);
+		// echo '</pre>';
 		$json = json_decode($response, true);
 		
 		if(!empty($json['error'])){
@@ -81,6 +81,90 @@ class Hagane {
 		} elseif(!empty($json['success'])){
 			unset($json['success']);
 			return $json['message'];
+		}
+	}
+}
+
+class HaganeAPI {
+	private static $instance;
+	protected $parentApiUrl;
+
+	public static function getInstance()
+	{
+		if (null === static::$instance) {
+			static::$instance = new static();
+		}
+		return static::$instance;
+	}
+
+	protected function  __construct(){
+	}
+	private function __clone(){
+	}
+	private function __wakeup(){
+	}
+
+	public function setApiUrl($parentApiUrl) {
+		$this->parentApiUrl = $parentApiUrl;
+	}
+
+	public function getApiUrl() {
+		return $this->parentApiUrl;
+	}
+}
+
+class Post extends HaganeAPI {
+	private $success;
+	private $message;
+	private $apiurl;
+
+	public function __construct($url, $params, $accessToken = null) {
+		//$this->apiurl = $apiurl;
+		$parent = \Hagane\HaganeAPI::getInstance();
+		$this->apiurl = $parent->getApiUrl();
+
+		if(substr($url, 0, 1) == '/'){
+			$url = $this->apiurl . $url;
+		} else {
+			$url = $this->apiurl . '/' . $url;
+		}
+
+		if (!empty($accessToken)) {
+			$params['accessToken'] = $accessToken;
+		}
+		
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $url,		
+			CURLOPT_USERAGENT => 'Hagane PHP SDK',
+			CURLOPT_POST => 1,
+			CURLOPT_POSTFIELDS => json_encode($params)
+		));
+		$response = curl_exec($curl);
+		// echo '<pre>';
+		// echo $url.'<br>';
+		// var_dump($response);
+		// echo '</pre>';
+		$json = json_decode($response, true);
+		
+		if(!empty($json['error'])){
+			//return json_encode($json['error']);
+			$this->message = json_encode($json['error']);
+			$this->success = false;
+		} elseif(!empty($json['success'])){
+			unset($json['success']);
+			//return $json['message'];
+			$this->message = $json['message'];
+			$this->success = true;
+		}
+	}
+
+	public function then($success, $failed = null) {
+		if ($this->success) {
+			$success($this->message);
+		} else {
+			$failed($this->message);
 		}
 	}
 }
