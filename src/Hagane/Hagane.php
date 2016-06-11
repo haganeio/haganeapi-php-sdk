b4169473c8df40fd27e546ad4d9faf9bcb3c11a2
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-06-11 14:53:53 -0500

    se agrega funcion delete a sdk

--- src/Hagane/Hagane.php
+++ src/Hagane/Hagane.php
@@ -93,6 +93,55 @@ class Hagane {
 			return $json['message'];
 		}
 	}
+
+	public function delete($url, $accessToken = null, $getparams = null){
+		if(substr($url, 0, 1) == '/'){
+			$url = $this->apiurl . $url;
+		} else {
+			$url = $this->apiurl . '/' . $url;
+		}
+
+		if (!empty($accessToken)) {
+			$url .= '?';
+			if (!empty($getparams)) {
+				foreach ($getparams as $key => $param) {
+					$url .= $key.'='.$param.'&';
+				}
+			}
+			$url .= 'accessToken='.$accessToken;
+		} else {
+			$url .= '?';
+			if (!empty($getparams)) {
+				foreach ($getparams as $key => $param) {
+					$url .= $key.'='.$param.'&';
+				}
+			}
+			$url = substr($url, 0, -1);
+		}
+
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $url,		
+			CURLOPT_CUSTOMREQUEST => 'DELETE',
+			CURLOPT_USERAGENT => 'Hagane PHP SDK'
+		));
+		$response = curl_exec($curl);
+		if ($this->debug) {
+			echo '<pre>';
+			echo $url.'<br>';
+			var_dump($response);
+			echo '</pre>';
+		}
+		$json = json_decode($response, true);
+		
+		if(!empty($json['error'])){
+			return json_encode($json['error']);
+		} elseif(!empty($json['success'])){
+			//unset($json['success']);
+			return $json['message'];
+		}
+	}
 }
 
 class HaganeAPI {
